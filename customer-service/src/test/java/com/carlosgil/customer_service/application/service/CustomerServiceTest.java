990ac5f39de1f87c748c820281cc63eca4e19a6a
csiz <carlosgilmunoz87@gmail.com>
2026-08-11 13:54:35 +0200

    feat(customer-service) RED create first test case

--- customer-service/src/test/java/com/carlosgil/customer_service/application/service/CustomerServiceTest.java
+++ customer-service/src/test/java/com/carlosgil/customer_service/application/service/CustomerServiceTest.java
@@ -1,8 +1,40 @@
 package com.carlosgil.customer_service.application.service;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class CustomerServiceTest {
 
-    void createCustomer_WithValidData_ShouldCreateCustomer(){
+    @InjectMocks
+    private CustomerService customerService;
 
+    @Mock
+    private CustomerRepository customerRepository;
+
+    @Test
+    void createCustomer_WithValidData_ShouldCreateCustomer(){
+        // GIVEN mejor usar pattern builder
+
+        Customer customer = new Customer();
+        customer.name("Carlos");
+        customer.surName("Gil");
+        customer.email("carlos.gil@example.com");
+
+        when(customerRepository.save(any(Customer.class))).thenReturn(customer);
+        // WHEN
+        Customer result = customerService.createCustomer(customer);
+
+
+
+        // THEN (Entonces se verifica que el resultado sea el esperado)
+        assertNotNull(result);
     }
 }

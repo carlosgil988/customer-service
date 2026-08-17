7a46b46a014e55a3ec633dbdc1db8dd07cfa3ec6
csiz <carlosgilmunoz87@gmail.com>
2026-08-17 16:55:50 +0200

    feat (customer-service) add customer mapper red test

---
+++ customer-service/src/main/java/com/carlosgil/customer_service/infrastructure/mapper/CustomerMapper.java
@@ -0,0 +1,6 @@
+package com.carlosgil.customer_service.infrastructure.mapper;
+
+public class CustomerMapper {
+
+
+}
---
+++ customer-service/src/test/java/com/carlosgil/customer_service/mapper/CustomerMapperTest.java
@@ -0,0 +1,35 @@
+package com.carlosgil.customer_service.mapper;
+
+import com.carlosgil.customer.api.model.Customer;
+import com.carlosgil.customer_service.infrastructure.mapper.CustomerMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class CustomerMapperTest {
+
+    private final CustomerMapper customerMapper = new CustomerMapper();
+
+    @Test
+    void shouldMapCustomerModelToCustomerApi() {
+        com.carlosgil.customer_service.domain.model.Customer customer =
+                new com.carlosgil.customer_service.domain.model.Customer();
+        customer.setId(3);
+        customer.setName("Carlos");
+        customer.setSurname("Gil");
+        customer.setEmail("carlosgilmunoz87@gmail.com");
+
+        // WHEN
+        Customer customerMapperResult =  customerMapper.mapToApi(customer);
+
+        assertNotNull(customerMapper);
+        assertEquals(3, customerMapperResult.getId());
+        assertEquals("Carlos", customerMapperResult.getName());
+        assertEquals("Gil", customerMapperResult.getSurname());
+        assertEquals("carlosgilmunoz87@gmail.com", customerMapperResult.getEmail());
+
+    }
+}
